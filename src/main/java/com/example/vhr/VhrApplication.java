ba32d214ef6ebb18798e119c59231f40a0ca33da
wangxiaochao <wangxiaochao@ebaolife.com>
2022-10-03 10:50:57 +0800

    init

---
+++ src/main/java/com/example/vhr/VhrApplication.java
@@ -0,0 +1,13 @@
+package com.example.vhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrApplication.class, args);
+    }
+
+}
